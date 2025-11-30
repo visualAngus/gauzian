b190ef442de66249cf47dc8b41155f1342f812f0
visualAngus <gael.pupin@gmail.com>
2025-11-30 12:23:30 +0100

    bouh

--- gauzian_front/scripts/main.js
+++ gauzian_front/scripts/main.js
@@ -1,18 +1,19 @@
 const bouton = document.getElementsByClassName('test')[0];
 bouton.addEventListener('click', function() {
-    fetch('https://192.168.1.165:3000/auth/autologin', {
+    fetch('https://gauzian.pupin.fr/auth/autologin', {
         method: 'POST',
         credentials: 'include', // <--- envoie/retourne les cookies cross-origin
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ /* vos données ici */ })
     })
-    .then(response => { /* traiter la réponse */ })
+    .then(response => response.json()) // Ceci ne plantera plus
+    .then(data => console.log(data))   // Affiche { status: "success", ... }
     .catch(err => { /* gestion d'erreur */ });
 });
 
 const loginButton = document.getElementsByClassName('login')[0];
 loginButton.addEventListener('click', function() {
-    fetch('https://192.168.1.165:3000/auth/login', {
+    fetch('https://gauzian.pupin.fr/auth/login', {
         method: 'POST',
         credentials: 'include',
         headers: { 'Content-Type': 'application/json' },
