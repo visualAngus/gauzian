291542de35218e22d537e768c7bca844602193e2
Gael Pupin <gael@pupin.fr>
2025-12-09 10:41:38 +0100

    Update file size limit to 120MB for uploads in Drive component

--- gauzian_front2/pages/drive.js
+++ gauzian_front2/pages/drive.js
@@ -227,8 +227,8 @@ export default function Drive() {
       const reader = new FileReader();
       console.log('Reading file:', selectedFile.name);
 
-      if (selectedFile.size > 1000 * 1024 * 1024) {
-        throw new Error('Le fichier est trop volumineux. Taille maximale autorisée : 1Go.');
+      if (selectedFile.size > 120 * 1024 * 1024) {
+        throw new Error('Le fichier est trop volumineux. Taille maximale autorisée : 120Mo.');
       }
       reader.onload = async (event) => {
         console.log('File read successfully, starting encryption...');
