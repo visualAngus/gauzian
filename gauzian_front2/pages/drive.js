9526a82e274f6a4582a6a3633cb08bbdc7ea29ac
Gael Pupin <gael@pupin.fr>
2025-12-09 10:48:28 +0100

    Update file size limit for uploads to 50MB in Drive component

--- gauzian_front2/pages/drive.js
+++ gauzian_front2/pages/drive.js
@@ -227,8 +227,8 @@ export default function Drive() {
       const reader = new FileReader();
       console.log('Reading file:', selectedFile.name);
 
-      if (selectedFile.size > 120 * 1024 * 1024) {
-        throw new Error('Le fichier est trop volumineux. Taille maximale autorisée : 120Mo.');
+      if (selectedFile.size > 50 * 1024 * 1024) {
+        throw new Error('Le fichier est trop volumineux. Taille maximale autorisée : 50Mo.');
       }
       reader.onload = async (event) => {
         console.log('File read successfully, starting encryption...');
