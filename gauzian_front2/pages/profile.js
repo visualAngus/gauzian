674c036f409011bb482a23b1a335233f627de166
visualAngus <gael.pupin@gmail.com>
2025-12-14 14:57:54 +0100

    chore: Comment out Gauzial component in profile header for future reference

--- gauzian_front2/pages/profile.js
+++ gauzian_front2/pages/profile.js
@@ -125,7 +125,7 @@ export default function ProfilePage() {
             <header className="header">
                 <div className="header-content">
                     <div className="logo-section">
-                        <Gauzial />
+                        {/* <Gauzial /> */}
                         <h1>Gauzian</h1>
                     </div>
                     <nav className="nav-menu">
