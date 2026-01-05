060b180c8af031b2437cb37d15c6279d4ba545e7
Gael Pupin <gael@pupin.fr>
2026-01-05 07:56:26 +0100

    Refactor code structure for improved readability and maintainability

--- gauzian_back/target/debug/deps/gauzian_back-eea5b41b0178e8d4.d
+++
@@ -1,5 +0,0 @@
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/gauzian_back-eea5b41b0178e8d4.d: src/main.rs
-
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/libgauzian_back-eea5b41b0178e8d4.rmeta: src/main.rs
-
-src/main.rs:
--- gauzian_back/target/debug/deps/untrusted-bed007caaf073043.d
+++
@@ -1,8 +0,0 @@
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/untrusted-bed007caaf073043.d: /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/lib.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/input.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/no_panic.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/reader.rs
-
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/libuntrusted-bed007caaf073043.rmeta: /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/lib.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/input.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/no_panic.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/reader.rs
-
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/lib.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/input.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/no_panic.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/untrusted-0.9.0/src/reader.rs:
