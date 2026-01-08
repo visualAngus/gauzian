195fa4dd1d1aae98cb94d0626e32cf6a03848cd8
visualAngus <gael.pupin@gmail.com>
2026-01-08 14:36:53 +0100

    feat: add button to navigate to Info page and standardize button styles across pages

--- gauzian_front/app/pages/index.vue
+++ gauzian_front/app/pages/index.vue
@@ -5,6 +5,8 @@
     <main>
       <button @click="autologin">Auto-login</button>
 	  <button @click="navigate('/drive')">Go to Drive</button>
+	  <button @click="navigate('/info')">Go to Info</button>
+
     </main>
 </template> 
 
@@ -38,6 +40,19 @@ body {
 	padding: 0;
 	box-sizing: border-box;
 	user-select: none;
+}
+
+button {
+    padding: 10px 15px;
+	background-color: #333333;
+	color: white;
+	border: none;
+	border-radius: 4px;
+	font-size: 14px;
+	font-family: "Montserrat", sans-serif;
+	font-weight: 600;
+	cursor: pointer;
+	margin-top: 5px;
 }
 
 </style>
