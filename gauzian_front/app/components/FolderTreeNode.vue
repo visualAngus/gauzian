5c2a47c9ebd4ddb1626ab855288cdec18494cda5
Gael Pupin <gael@pupin.fr>
2026-01-14 13:06:05 +0100

    feat: add FolderTreeNode component for improved folder structure representation

---
+++ gauzian_front/app/components/FolderTreeNode.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="folder-three" :data-folder-id="node.folder_id">
+    <div class="div_name_bnt" :class="{ 'is-active-folder': activeId === node.folder_id }">
+      <button class="tree-toggle" @click.stop="$emit('toggle', node)">
+        <span v-if="node.isLoading">⋯</span>
+        <span v-else-if="(node.children?.length ?? 0) > 0 || !node.isLoaded">
+          {{ node.isExpanded ? '▾' : '▸' }}
+        </span>
+        <span v-else>&nbsp;</span>
+      </button>
+      <a
+        class="three-folder-name"
+        :class="{ select_folder_three: activeId === node.folder_id }"
+        @click.prevent="$emit('select', node)"
+      >
+        {{ node.metadata?.folder_name || "Dossier sans nom" }}
+      </a>
+    </div>
+    <div class="div_enfants" v-show="node.isExpanded">
+      <FolderTreeNode
+        v-for="child in node.children"
+        :key="child.folder_id"
+        :node="child"
+        :active-id="activeId"
+        @select="$emit('select', $event)"
+        @toggle="$emit('toggle', $event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  node: {
+    type: Object,
+    required: true,
+  },
+  activeId: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(['select', 'toggle']);
+</script>
--- gauzian_front/app/pages/drive.vue
+++ gauzian_front/app/pages/drive.vue
@@ -218,11 +218,11 @@ import {
   nextTick,
   onMounted,
   onBeforeUnmount,
-  defineComponent,
 } from "vue";
 import { useHead } from "#imports"; // Nécessaire si tu es sous Nuxt, sinon à retirer
 import dropzone from "~/directives/dropzone";
 import FileItem from "~/components/FileItem.vue";
+import FolderTreeNode from "~/components/FolderTreeNode.vue";
 
 const vDropzone = dropzone;
 definePageMeta({
@@ -240,56 +240,6 @@ import {
 } from "~/utils/crypto";
 
 const API_URL = "https://gauzian.pupin.fr/api";
-
-const FolderTreeNode = defineComponent({
-  name: "FolderTreeNode",
-  props: {
-    node: {
-      type: Object,
-      required: true,
-    },
-    activeId: {
-      type: String,
-      required: true,
-    },
-  },
-  emits: ["select", "toggle"],
-  setup(props, { emit }) {
-    const onToggle = () => emit("toggle", props.node);
-    const onSelect = () => emit("select", props.node);
-    return { onToggle, onSelect };
-  },
-  template: `
-    <div class="folder-three" :data-folder-id="node.folder_id">
-      <div class="div_name_bnt" :class="{ 'is-active-folder': activeId === node.folder_id }">
-        <button class="tree-toggle" @click.stop="onToggle">
-          <span v-if="node.isLoading">...</span>
-          <span v-else-if="(node.children?.length ?? 0) > 0 || !node.isLoaded">
-            {{ node.isExpanded ? "v" : ">" }}
-          </span>
-          <span v-else>.</span>
-        </button>
-        <a
-          class="three-folder-name"
-          :class="{ select_folder_three: activeId === node.folder_id }"
-          @click.prevent="onSelect"
-        >
-          {{ node.metadata?.folder_name || "Dossier sans nom" }}
-        </a>
-      </div>
-      <div class="div_enfants" v-show="node.isExpanded">
-        <FolderTreeNode
-          v-for="child in node.children"
-          :key="child.folder_id"
-          :node="child"
-          :active-id="activeId"
-          @select="$emit('select', $event)"
-          @toggle="$emit('toggle', $event)"
-        />
-      </div>
-    </div>
-  `,
-});
 
 const etat = ref("login");
 const loading = ref(false);
