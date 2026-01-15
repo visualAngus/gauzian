ae1b35dc8f197ef61169dcb74c9c40af0b69788e
Gael Pupin <gael@pupin.fr>
2026-01-15 17:28:15 +0100

    Add finalize_download endpoint, remove unreliable Drop-based cleanup

--- gauzian_back/src/routes.rs
+++ gauzian_back/src/routes.rs
@@ -20,6 +20,7 @@ pub fn app(state: AppState) -> Router {
         .route("/drive/get_folder/{folder_id}", get(handlers::get_folder_handler))
         .route("/drive/file/{file_id}", get(handlers::get_file_info_handler))
         .route("/drive/download/{file_id}", get(handlers::download_file_handler))
+        .route("/drive/finalize_download", post(handlers::finalize_download_handler))
         .route("/drive/download_chunk/{s3_key}", get(handlers::download_chunk_handler))
         .route("/drive/folder_contents/{folder_id}", get(handlers::get_folder_contents_handler))
         .route("/drive/abort_upload", post(handlers::abort_upload_handler))
