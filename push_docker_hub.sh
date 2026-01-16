9721ee49c1534e082e9e2bac764145e83446e154
visualAngus <gael.pupin@gmail.com>
2026-01-16 17:03:12 +0100

    feat: Implement download cancellation and progress management
    - Added support for cancelling ongoing downloads using AbortController.
    - Enhanced downloadFile and downloadFolderAsZip functions to handle cancellation.
    - Cleaned up download progress tracking upon cancellation or completion.
    - Introduced a new script to automate Docker image builds and pushes for backend and frontend.

---
+++ push_docker_hub.sh
@@ -0,0 +1,2 @@
+docker build -t angusvisual/gauzian-backend:dev gauzian_back && docker push angusvisual/gauzian-backend:dev
+docker build -t angusvisual/gauzian-front:dev gauzian_front && docker push angusvisual/gauzian-front:dev
