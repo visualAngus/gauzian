25b1152aeaebc068d3cb6a5f21c071ab6f500876
visualAngus <gael.pupin@gmail.com>
2026-02-02 08:16:25 +0100

    Remove generated debug dependency files for tokio_macros, tower, tracing, and tracing_core

--- gauzian_back/target/debug/deps/serde_path_to_error-6d17e846e29d92bc.d
+++
@@ -1,9 +0,0 @@
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/serde_path_to_error-6d17e846e29d92bc.d: /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/lib.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/de.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/path.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/ser.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/wrap.rs
-
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/libserde_path_to_error-6d17e846e29d92bc.rmeta: /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/lib.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/de.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/path.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/ser.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/wrap.rs
-
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/lib.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/de.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/path.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/ser.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_path_to_error-0.1.20/src/wrap.rs:
--- gauzian_back/target/debug/deps/tokio_macros-cf78e94a20c18cea.d
+++
@@ -1,7 +0,0 @@
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/tokio_macros-cf78e94a20c18cea.d: /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/lib.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/entry.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/select.rs
-
-/home/gael/Bureau/gauzian/gauzian_back/target/debug/deps/libtokio_macros-cf78e94a20c18cea.so: /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/lib.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/entry.rs /home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/select.rs
-
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/lib.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/entry.rs:
-/home/gael/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/select.rs:
